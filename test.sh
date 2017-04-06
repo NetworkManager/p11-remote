cc6ce04a83b54787b9ee6e2dce1ca9185fbfb6b6
Lubomir Rintel <lkundrak@v3.sk>
2017-04-06 19:14:50 +0200

    test: various improvements
    Will be useful for further tests.

--- test.sh
+++ test.sh
@@ -7,10 +7,25 @@
 #   Provides p11tool to manipulate the token
 
 TOKEN='pkcs11:model=1.0;manufacturer=Gnome%20Keyring;serial=1%3aUSER%3aDEFAULT;token=Gnome2%20Key%20Storage'
-PRIVKEY=test.key
+
+set -e
+
+do_check ()
+{
+	rm -f test issue
+
+	# Execute
+	tee /dev/stderr |$* openssl
+
+	# Decrypt with the default RSA engine
+	openssl rsautl -decrypt -inkey test.key -in test -out issue
+
+	# Check if the files are different
+	diff /etc/issue issue
+}
 
 # Pre-test fixture clean up
-rm -f test issue test.key
+rm -f test.key
 p11tool --batch --login --delete "$TOKEN;object=Test-Server" 2>/dev/null || :
 
 # Generate a test key
@@ -20,20 +35,13 @@ openssl genrsa >test.key
 p11tool --batch --login --label=Test-Server --write --load-privkey=test.key "$TOKEN"
 
 # Perform encryption
-openssl <<EOF
+do_check <<EOF
 engine -t -pre SO_PATH:$PWD/.libs/libp11-kit-engine.so -pre LIST_ADD:1 -pre LOAD dynamic
 rsautl -engine pkcs11 -keyform engine -encrypt -inkey '$TOKEN;object=Test-Server;type=private;pin-value=' -in /etc/issue -out $PWD/test
 EOF
 
 # Drop the object from the token
 p11tool --batch --login --delete "$TOKEN;object=Test-Server" 2>/dev/null || :
-
-# Decrypt with the default RSA engine
-openssl \
-rsautl -decrypt -inkey test.key -in test -out issue
-
-# Check if the files are different
-diff /etc/issue issue
 
 # Cleanup
 rm -f test issue test.key
